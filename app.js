99206d39d4d130469dfc0bd77608dce61e4f39b9
Nikhil More <nikhilmore2011@hotmail.com>
2021-02-18 09:38:27 +0100

    initial

---
+++ app.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('form').onsubmit = function()
+    {
+            //GET request to URL
+            fetch('https://api.exchangeratesapi.io/latest?base=USD')
+            .then(response => response.json())
+            .then(data => {
+                const currency = document.querySelector('#currency').value.toUpperCase();
+
+                const rate =  data.rates[currency];
+
+                if(rate !==undefined) {
+                    document.querySelector('#result').innerHTML = `1 USD is equal to ${rate.toFixed(2)} ${currency}`
+                } else {
+                    document.querySelector('#result').innerHTML = 'Invalid Currency!';
+                }
+
+            })
+            .catch (error => {
+                console.log('Error:', error);
+            })
+
+            return false;
+    }
+})
